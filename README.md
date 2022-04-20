0d683f06f6ac24f05a37eda8bb52c90fd51646a5
Sour <79670011+Sourlol@users.noreply.github.com>
2022-04-19 20:25:45 -0400

    Mobile formatting update (buggy)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Sourdev.com Source Code
+# `Sourdev.com` Source Code
 
 This is my source code for my website. If you find any bugs, please create an issue and describe the bug. If you have have any suggestiongs, please email be at `sour@sourdev.com`.
+
+Feel free to also DM me on Discord at `Sour#0001`.
--- home/main.js
+++ home/main.js
@@ -1,11 +1,6 @@
-const menuBtn = document.querySelector(".dropdown_mobile button");
 const homeBtn = document.getElementById('homeBtn');
 
 document.body.style.zoom = "100%";
-
-menuBtn.addEventListener("click", function () {
-    this.classList.toggle("active");
-});
 
 homeBtn.addEventListener("click", function () {
     location.href = '/home';
